03af27f13abf1f5737ec1ad1ca396f917b68651c
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-02-06 21:10:58 +0530

    Create Love Calculator.js

---
+++ Love Calculator.js
@@ -0,0 +1,11 @@
+var name1 = prompt("Enter your name");
+var name2 = prompt("Enter your partner's name");
+
+
+var n = Math.random();
+n = Math.floor(n*101);
+if(n>50){
+    alert( "Ohh Maa Goo Turu LUB \n" + name1 +" & "+ name2 +" your love score is: "+ n );
+}
+else{
+    alert( " Tum Rehne Do, Single hi accha h just like me bcz \n" + name1 +" & "+ name2 +" your love score is: "+ n );
