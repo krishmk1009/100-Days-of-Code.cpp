7875565ea4bb5e49696fe495867c40c76d2174b1
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-02-26 16:50:28 +0530

    Create 204. Count Primes.cpp

---
+++ 204. Count Primes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+     
+        int count = 0;
+        
+        vector<bool> arr(n+1,true);
+        
+        for( int i = 2;i<n;i++){
+            
+            if(arr[i]){
+                
+                count ++;
+                
+                for(int j = 2*i ; j<n; j = j+i){
+                    arr[j]= false;
+                    
+                }
+            }
+            
+        }
+        return count;
+    }
+};
