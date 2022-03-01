a079067456d59999065c3c6949bfa800b38a3870
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-03-01 16:00:48 +0530

    Create Basic-Pointer-COde-studio-1to6.cpp

---
+++ Basic-Pointer-COde-studio-1to6.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	
+//	Code Studio's basic pointers question from 1 - 6
+	
+/*	
+	float f = 10.5;
+float p = 2.5;
+float* ptr = &f;
+(*ptr)++;
+*ptr = p;
+cout << *ptr << " " << f << " " << p;
+return 0;
+ans 2.5 2.5 2.5
+*/
+
+
+/*
+int a = 7;
+int b = 17;
+int *c = &b; 
+a = *c;
+*c = *c + 1;
+cout  << a << "  " << b << endl;
+ans -- 17 18
+*/
+
+
+
+
+/*
+int a = 7;
+int b = 17;
+int *c = &b;
+*c = 7;
+cout  << a << " " << b << endl;
+ans -- 7 7
+*/
+
+
+
+
+/*
+int *ptr = 0;
+int a = 10;
+*ptr = a;
+ cout << *ptr << endl;
+ ans  - none
+*/
+
+/*
+int b = 10;
+int *a = &b;
+ans -- cout<<a;
+*/
+
+/*
+char ch = 'a';
+ char* ptr = &ch;
+ ch++;
+ cout << *ptr << endl;
+ 
+ ans -- b;  
+ */
+ 
+ 
+ return 0;
+}
