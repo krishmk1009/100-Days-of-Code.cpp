0cbbfa30cb87cf7cc5e85b9254d771342afdec23
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-07-21 22:56:47 +0530

    Create Inheritance_access_modifiier.cpp

---
+++ Inheritance_access_modifiier.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class Player
+{
+protected:
+    int age;
+
+public:
+    string name;
+
+public:
+    void setName(string n)
+    {
+        this->name = n;
+    }
+    void setAge(int a)
+    {
+        this->age = a;
+    }
+};
+
+class Batsman : public Player
+{
+public:
+    char gender;
+
+    void score()
+    {
+        cout << "batsman is sleeping" << endl;
+    }
+
+    void setNameOfBatsman(string n)
+    {
+        this->name = n;
+    }
+    void setAgeOfBatsman(int a)
+    {
+        this->age = a;
+    }
+
+};
+
+int main()
+{
+
+    Batsman b;
+    // cout<<b.age;
+
+    // b.setAgeOfBatsman(15);
+    // cout<<b.name;
+    // b.setNameOfBatsman("mk");
+
+    // b.setAge(20);
+    // cout << b.age << endl;
+    // b.setName("mk");
+    // cout << b.name << endl;
+    // b.score();
+    return 0;
+}
