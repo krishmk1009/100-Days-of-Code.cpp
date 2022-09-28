d44379c9783bc8dbe820ee51cbcc61d5ef038f8d
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-09-28 13:30:23 +0530

    Create Build_tree_using_level_order_traversal.cpp

---
+++ Binary_tree/Build_tree_using_level_order_traversal.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void buildTreeLevelOrder(Node *&root)
+{
+    queue<Node *> q;
+    cout << "Enter the data for root node: " << endl;
+    int data;
+    cin >> data;
+    root = new Node(data);
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        cout << "Enter data for left node of " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if (leftData != -1)
+        {
+            temp->left = new Node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter data for right node of " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if (rightData != -1)
+        {
+            temp->right = new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    cout << "Final Tree: " << endl;
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main()
+{
+    Node *root = NULL;
+    buildTreeLevelOrder(root);
+    
+    levelOrderTraversal(root);
+
+        return 0;
+}
+
+// 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+// 1 2 4 -1 -1 5 -1 -1 3 -1 -1
+// 1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1
