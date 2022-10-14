3eda6637fc54a438c7fa49775f07114654410821
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-10-14 13:26:36 +0530

    Update map_in_desc_order.cpp

--- hash_map/map_in_desc_order.cpp
+++ hash_map/map_in_desc_order.cpp
@@ -8,7 +8,7 @@ int main(){
     
     int arr[] = {4,3,6,9,12,3};
     int n = 6;
-    map<int,int> mp;
+    map<int,int,greater<int>> mp;
     
     for(int i = 0 ; i<n;i++){
         mp.insert({arr[i] , i}); 
@@ -17,7 +17,7 @@ int main(){
     map<int,int>:: iterator rt;
     
     
-    for(auto rt = mp.rbegin() ; rt!=mp.rend() ; rt++){
+    for(auto rt = mp.begin() ; rt!=mp.end() ; rt++){
         cout<<rt->first<<" => "<<rt->second<<endl;
     }
     return 0;
