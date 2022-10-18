a56d48cd237483db74b7100b310c9089f4f2ebcf
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-10-18 11:34:43 +0530

    Create count_items_matching_rule.cpp

---
+++ count_items_matching_rule.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int ans = 0 ;
+    int index = -1;
+        if(ruleKey == "type" ){
+            index = 0;
+        }
+        else if(ruleKey == "color"){
+            index = 1;
+        }
+        else{
+            index = 2;
+        }
+        
+        
+        for(int i = 0 ; i<items.size() ; i++){
+            
+            if(items[i][index] == ruleValue){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
