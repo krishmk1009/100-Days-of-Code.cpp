f88ff3624e5e42c58625fac99098f6d5e8bcb441
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-12-10 12:53:01 +0530

    Create merge_string_alternately.cpp

---
+++ merge_string_alternately.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans;
+        int i =0;
+        while(i<word1.length() || i<word2.length()){
+            if(i<word1.length() && i<word2.length()){
+                    ans+=word1[i];
+                    ans+=word2[i];
+            }
+
+            else if(i>=word1.length()){
+                ans+=word2[i];
+              
+            }
+            else{
+               ans+=word1[i]; 
+            }
+            i++;
+        }
+
+        return ans;
+    }
+};
