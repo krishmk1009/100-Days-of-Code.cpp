404e9ca346db1eb1451c373c9111a31fe0e2ef81
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-02-16 13:07:35 +0530

    Create 1636. Sort Array by Increasing Frequency.cpp

---
+++ 1636. Sort Array by Increasing Frequency.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+static bool comp(pair<int,int>a, pair<int,int>b){
+    return a.second<b.second;
+}
+    vector<int> frequencySort(vector<int>& nums) {
+        vector<pair<int,int>> v;
+        vector<int>ans;
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        
+        for(auto it:mp){
+            v.push_back({it.first,it.second});
+        }
+
+        sort(v.begin() , v.end());
+        reverse(v.begin() , v.end());
+        stable_sort(v.begin() , v.end() , comp);
+
+        for(int i=0;i<v.size();i++){
+            for(int j=0;j<v[i].second;j++){
+                ans.push_back(v[i].first);
+            }
+            
+        }
+
+        return ans;
+    }
+};
