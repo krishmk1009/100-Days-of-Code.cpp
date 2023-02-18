648d9cc55757cb5bed5747b697069a1f857efa0c
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-02-18 13:52:19 +0530

    Create sort_color_optimised_map.cpp

---
+++ DSA_SHEET/sort_color_optimised_map.cpp
@@ -0,0 +1,13 @@
+
+        map<int,int> mp;
+        for(int i=0;i<nums.size() ; i++){
+            mp[nums[i]]++;
+        }
+        int i=0;
+        for(auto it:mp){
+            while(it.second>0){
+                nums[i++] = it.first;
+                it.second--;
+            }
+            
+        }
