65406668d67130d81e5ddea6f4e1439bc71eee35
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-02-19 20:09:35 +0530

    Create subArray_sums_divisible_by_k_brute_force.cpp

---
+++ subArray_sums_divisible_by_k_brute_force.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        long long ans = 0;
+
+
+        for(int i=0; i<nums.size();i++){
+
+            long long currentSum = 0;
+            for(int j=i ; j<nums.size() ; j++){
+                currentSum+= nums[j];
+                if(currentSum %k ==0){
+                    ans++;
+                }
+            }
+
+        }
+        return ans;
+    }
+};
+
+
+
+// O(n^2)
