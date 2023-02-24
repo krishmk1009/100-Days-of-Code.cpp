51cbd5f10c3fb70199f7c75bef73e27f49f0ca1e
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-02-24 13:49:48 +0530

    Create 122. Best Time to Buy and Sell Stock II_short.cpp

---
+++ DSA_SHEET/122. Best Time to Buy and Sell Stock II_short.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int buy =prices[0];
+
+       for(int i=0;i<prices.size()-1 ;i++){
+
+           if(buy<prices[i+1]){
+               ans+= prices[i+1]- buy;       
+           }
+           buy = prices[i+1];
+
+       }
+        return ans;
+    }
+};
