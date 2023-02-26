b7d8d031ea23a6ff4e5c613de77c768dfa70842e
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-02-26 12:19:47 +0530

    Create 2574. Left and Right Sum Differences.cpp

---
+++ 2574. Left and Right Sum Differences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> leftRigthDifference(vector<int>& nums) {
+        
+        vector<int>left(nums.size() , 0);
+        vector<int>right(nums.size() ,0);
+        int leftSum = 0;
+        int rightSum = 0;
+        int j = nums.size()-1;
+        for(int i=0;i<nums.size() ; i++){
+            left[i] = leftSum;
+            leftSum += nums[i];
+
+            right[j] = rightSum;
+            rightSum += nums[j--];
+
+
+        }
+        for(int i=0;i<nums.size() ; i++){
+            nums[i] = abs(left[i] -right[i]);
+        }
+
+        return nums;
+    }
+};
