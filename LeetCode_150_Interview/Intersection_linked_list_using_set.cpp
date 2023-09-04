57c781894fe0bdd800751e41f3bdf29ad52dd7ab
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-09-04 18:48:08 +0530

    Create Intersection_linked_list_using_set.cpp

---
+++ LeetCode_150_Interview/Intersection_linked_list_using_set.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+       set<ListNode*>s;
+
+       ListNode*temp = headA;
+
+       while(temp!=NULL){
+           s.insert(temp);
+           temp=temp->next;
+       }
+
+       temp = headB;
+
+       while(temp!=NULL){
+           if(s.find(temp) !=s.end()){
+               return temp;
+           }
+
+           temp= temp->next;
+       }
+
+
+return NULL;
+
+       
+
+        
+    }
+};
