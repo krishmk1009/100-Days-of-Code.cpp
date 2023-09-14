15f4920900f4e5559bedf89f248549db03874024
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-09-14 13:16:37 +0530

    Create union_of_arrays.cpp

---
+++ union_of_arrays.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        // Your code goes here
+        int low = 0;
+        int high = n-1;
+        
+        while(low<high){
+            
+            if(arr[low] <0){
+                low++;
+            }
+            else if(arr[high]>0){
+                high--;
+            }
+            else{
+                swap(arr[low] , arr[high]);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		ob.segregateElements(a,n);
+		
+        for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+		cout<<endl;
+	}
+}
+
+// } Driver Code Ends
