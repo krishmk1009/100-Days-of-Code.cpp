9b2032e24aa79d65c2dae7d9d6f20b6d03949499
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-09-24 19:02:56 +0530

    Create 326. Power of Three.cpp

---
+++ LeetCode_150_Interview/326. Power of Three.cpp
@@ -0,0 +1,15 @@
+326. Power of Three
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+
+     if(n ==1){
+         return 1;
+     }
+
+     if(n ==0 || n <0 || n%3 >0) return 0;
+
+     return isPowerOfThree(n/3);
+        
+    }
+};
